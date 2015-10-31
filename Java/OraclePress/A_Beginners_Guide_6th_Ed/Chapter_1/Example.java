4b11b672d3d7d9633fdb47daebd9ec1f5cac7b28
Alan K. Martinez <akmartinez1@yahoo.com>
2015-10-31 15:37:36 -0700

    Page 12 Example

---
+++ Java/OraclePress/A_Beginners_Guide_6th_Ed/Chapter_1/Example.java
@@ -0,0 +1,11 @@
+/*
+    Ths is a simple Java Program.
+
+    Calle this file Example.java.
+*/
+class Example {
+  // A Java program begins with a call to main().
+  public static void main(String args[]) {
+    System.out.println("Java drives the Web.");
+  }
+}
