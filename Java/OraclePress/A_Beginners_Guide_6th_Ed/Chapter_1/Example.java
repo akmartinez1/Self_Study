74ad71165c2c400ea4752ef800acbefd60ece7f4
Alan K. Martinez <akmartinez1@yahoo.com>
2015-10-31 21:26:13 -0700

    Chapter 1 Example
    Minor edit for test at command line.

--- Java/OraclePress/A_Beginners_Guide_6th_Ed/Chapter_1/Example.java
+++ Java/OraclePress/A_Beginners_Guide_6th_Ed/Chapter_1/Example.java
@@ -5,7 +5,7 @@
 */
 class Example {
   // A Java program begins with a call to main().
-  public static void main(String args[]) {
+  public static void main (String [] args) {
     System.out.println("Java drives the Web.");
   }
 }
